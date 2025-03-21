b373338bd1454fc76d21cac1b22f6ce64624c796
Shiela N. Llevares <llevaresshiela28@gmail.com>
2025-03-22 04:39:47 +0800

    Add files via upload

--- numberGuess.js
+++ numberGuess.js
@@ -2,7 +2,7 @@ let correctNumber = 7;
 let guess;
 
 while (guess !== correctNumber) {
-  guess = parseInt(prompt("Guess a number between 1 and 10:"));
+  guess = prompt("Guess a number between 1 and 10:");
 
   if (guess < correctNumber) {
     console.log("Too low!");
